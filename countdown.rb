8e12477489349243df01c990abdecf996738d777
HectorLopez1998 <hectorlopezp1998@gmail.com>
2020-11-10 12:56:34 -0500

    commit tree faa1b69bd74d74b3deb84b8ef856dcdfeac0b354

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(n)
+    while n > 0
+        puts("#{n} SECOND(S)!\n")
+        n -= 1
+        if n == 0
+            return "HAPPY NEW YEAR!"
+        end
+    end
+    
 end
+
+puts(countdown(10))
+
+def countdown_with_sleep(n)
+    while n > 0
+        puts("#{n} SECOND(S)!\n")
+        sleep 1
+        n -= 1
+        if n == 0
+            return "HAPPY NEW YEAR!"
+        end
+    end
+end
+
+puts countdown_with_sleep(5)
